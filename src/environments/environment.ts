122fd9db98aaf7b0b022d72fde3678921d40ea16
Garima Sharma <garima.sharma@zerfinity.com>
2026-02-06 15:54:41 +0530

    Removed config from environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,13 @@
-// src/environments/environment.ts
+
 export const environment = {
   production: false,
   firebase: {
-  apiKey: "AIzaSyAcH0zvdME2EuJm2w_OJt9qgbN1W1a5_as",
-  authDomain: "patient-5cba8.firebaseapp.com",
-  projectId: "patient-5cba8",
-  storageBucket: "patient-5cba8.firebasestorage.app",
-  messagingSenderId: "714758281495",
-  appId: "1:714758281495:web:86e107356abe31f8af3f52"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   }
 };
 
-// Replace the above values with your actual Firebase configuration
-// You can find these in Firebase Console > Project Settings > General > Your apps
