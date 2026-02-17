cec4d5c1a1e416b44d172bd08a97eeeab16f8d16
Garima Sharma <garima.sharma@zerfinity.com>
2026-02-17 20:16:09 +0530

    adding api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@
 export const environment = {
   production: false,
   firebase: {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyC1tWaoSCZLJ2iayhfL3dqOr2lb6IXjQW0",
+  authDomain: "intellirx-test.firebaseapp.com",
+  projectId: "intellirx-test",
+  storageBucket: "intellirx-test.firebasestorage.app",
+  messagingSenderId: "143213488063",
+  appId: "1:143213488063:web:e88b8d09640113750fe30a",
+  measurementId: "G-YPK8C0V09T"
   }
 };
 
